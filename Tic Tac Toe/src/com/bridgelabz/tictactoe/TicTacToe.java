655202c91b1e59a2da6686c6231009f6409a2d63
piyushkolhe94 <piyushkolhe94gmail.com>
2022-10-02 16:38:58 +0530

    created board of char size 10

--- Tic Tac Toe/src/com/bridgelabz/tictactoe/TicTacToe.java
+++ Tic Tac Toe/src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToe {
-    public static void main(String[] args) {
-        System.out.println("welcome to program");
+    public static void main(String[] args)
+         System.out.println("Welcome to Tic Tac Toe Game");
+
+    char []board=new char[]{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',};
     }
-}
+
