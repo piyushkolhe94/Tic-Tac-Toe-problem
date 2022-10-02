26f9268c77864af045e0f9e374db507f43cdbfc8
piyushkolhe94 <piyushkolhe94gmail.com>
2022-10-02 17:30:42 +0530

    play again and exit position

--- Tic Tac Toe/src/com/bridgelabz/tictactoe/TicTacToe.java
+++ Tic Tac Toe/src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -3,49 +3,73 @@ package com.bridgelabz.tictactoe;
 import java.util.Scanner;
 
 public class TicTacToe {
-    static void selectPosition(int number, char letter){
-        System.out.println(position+ " is already taken");
-        position=scanner.nextInt();
-    }else {
-        position=(int) ((Math.random()*10+1)%9);
-        position=getPosition();
+     public static void main(String[] args) throws InterruptedException {
+        System.out.println("1.Play again \n2.Exit");
+        resetGame(scanner.nextInt());
     }
-}
-            board[position] =letter;
-static int getCornerPosition(){
+
+
+};
+
+    }
+
+static char chooseLetter(){
+        char letter=' ';
+        while (letter != 'O' && letter != 'X'){
+ static void showBoard(){
+        System.out.println(board[4]+" | "+board[5]+" | "+board[6]);
+        System.out.println(board[7]+" | "+board[8]+" | "+board[9]);
         }
-        return position;
+
+static void selectPosition(int number, char letter){
+        int position=number;
+
+static void selectPosition(int number, char letter){
+        }else {
+        position=(int) ((Math.random()*10+1)%9);
         }
-static int getAvailablePosition(){
-        int position=(int)(Math.random()*10)%9+1;
-        while (board[position]!=' '){
-        position=(int)(Math.random()*10)%9+1;
+
         }
-        return position;
+        board[position] =letter;
         }
+ static int doToss(){
+        return COMPUTER;
+        }
+        }
+
+static void resetGame(int x){
+        if (x == 1) {
+        for (int i = 0; i < 10; i++) {
+ static void resetGame(int x){
+        moveCount=0;
+        showBoard();
+        }
+
+        }
+
+static int checkWinner(){
+        if (board[1] == board[2] && board[2] == board[3] && board[2] !=' ') {
+        return  (board[1] == playerLetter) ? PLAYER_1 : COMPUTER;
+ static int checkWinner(){
+        return  (board[3] == playerLetter) ? PLAYER_1 : COMPUTER;
+        }else return 0;
+        }
+
 static int getPosition(){
-        if ( board[1]==' ' && board[2]== computerSymbol && board[2] == board[3]
-        || board[1]==' ' && board[4]== computerSymbol && board[4] == board[7]
-static int getPosition(){
+        if ( board[1]==' ' && board[2]== computerLetter && board[2] == board[3]
+        || board[1]==' ' && board[4]== computerLetter && board[4] == board[7]
+ static int getPosition(){
+        || board[9]==' ' && board[3] == board[6] && board[6]!=' '
         || board[9]==' ' && board[7] == board[8] && board[8]!=' ') {
         return 9;
-        }else {
-        return getCornerPosition();
-        boolean isCornerAvailable=(board[1]==' ' || board[3]==' ' || board[7]==' ' || board[9]==' ');
-        boolean isCenterAvailable=(board[5]==' ');
-
-        if (isCornerAvailable) {
-        return getCornerPosition();
-        } else if (isCenterAvailable) {
-        return 5;
-        }else {
-        return getAvailablePosition();
-                    }
-                }
-            }
-        }
+        }else
+        return  (int) ((Math.random()*10+1)%9);
+        }else return  (int) ((Math.random()*10+1)%9);
     }
 }
+        }
+        }
+
 
 
 
